14654731d9dfb1df76c563de34881745b0822501
DiegodeSouza <diegodesouza.souza@gmail.com>
2023-10-02 15:57:10 -0300

    melhorias no codigo do desafio

--- entrega_site_projeto_p_avaliar/HTML_e_javaScript/script.js
+++ entrega_site_projeto_p_avaliar/HTML_e_javaScript/script.js
@@ -1,11 +1,14 @@
 //arquivo de exercicio de arquivo JS
 //impressão no console log
-let imprimir = () => console.log("Conexão foi um sucesso!!!")
-
+let imprimir = () => {
+    for(x=0;x<3;x++){
+        console.log("Conexão foi um sucesso!!!")
+    }
+}
 imprimir()
 
 
 //impressão direto na pagina
 let elemento = document.getElementById('conexaoJS')
-let imprimirHTML = () => "Conexão foi um Sucesso!!"
-elemento.innerHTML=imprimirHTML();
+let impressao="Conexão com arquivo JavaScript foi um sucesso!!!"+" \n"+ "Aperte F12 para ver no console"
+elemento.innerHTML = impressao
